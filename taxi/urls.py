77c702d1e014e467d27aff0f300cbb22fb0f2350
Alina Tkachenko <tkachenkoalinaarthurovna@gmail.com>
2026-05-20 12:45:48 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -55,8 +55,6 @@ urlpatterns = [
          name="driver-list"),
     path("drivers/<int:pk>/", DriverDetailView.as_view(),
          name="driver-detail"),
-    path("drivers/", DriverListView.as_view(),
-         name="driver-list"),
     path("drivers/<int:pk>/", DriverDetailView.as_view(),
          name="driver-detail"),
     path("drivers/create/", DriverCreateView.as_view(),
--- tests/test_views.py
+++ tests/test_views.py
@@ -52,3 +52,51 @@ class PrivateViewsTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "taxi/driver_list.html")
         self.assertIn("driver_list", response.context)
+
+    def test_search_manufacturer_by_name(self):
+        Manufacturer.objects.create(name="BMW", country="Germany")
+        Manufacturer.objects.create(name="Audi", country="Germany")
+
+        response = self.client.get(
+            reverse("taxi:manufacturer-list"),
+            {"name": "BMW"}
+        )
+
+        self.assertContains(response, "BMW")
+        self.assertNotContains(response, "Audi")
+
+    def test_search_car_by_model(self):
+        manufacturer = Manufacturer.objects.create(
+            name="BMW",
+            country="Germany"
+        )
+        Car.objects.create(model="X5", manufacturer=manufacturer)
+        Car.objects.create(model="A4", manufacturer=manufacturer)
+
+        response = self.client.get(
+            reverse("taxi:car-list"),
+            {"model": "X5"}
+        )
+
+        self.assertContains(response, "X5")
+        self.assertNotContains(response, "A4")
+
+    def test_search_driver_by_username(self):
+        Driver.objects.create_user(
+            username="john_driver",
+            password="12345",
+            license_number="ABC12345",
+        )
+        Driver.objects.create_user(
+            username="bob_driver",
+            password="12345",
+            license_number="DEF12345",
+        )
+
+        response = self.client.get(
+            reverse("taxi:driver-list"),
+            {"username": "john"}
+        )
+
+        self.assertContains(response, "john_driver")
+        self.assertNotContains(response, "bob_driver")
